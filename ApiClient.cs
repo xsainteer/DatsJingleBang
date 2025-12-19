fb0ac044317480bac9e95e854f6e39dff6fa8be0
beka-jpg <Hikitapredstavlenie@gmail.com>
2025-12-19 22:11:01 +0600

    simple http client

---
+++ ApiClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatsJingleBang;
+public class ApiClient
+{
+    private readonly HttpClient _httpClient;
+    private const string BASE_URL = "https://games.datsteam.dev/";
+    private const string TOKEN = GameConstants.Token;
+
+    public ApiClient()
+    {
+        _httpClient = new HttpClient();
+        _httpClient.BaseAddress = new Uri(BASE_URL);
+        _httpClient.DefaultRequestHeaders.Add("X-Auth-Token", TOKEN);
+    }
+}
