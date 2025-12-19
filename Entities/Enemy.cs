beffdc4c65f1ef35f0c2beb6cdc713e68a9ce2d6
beka-jpg <Hikitapredstavlenie@gmail.com>
2025-12-19 22:33:40 +0600

    enemy cs

---
+++ Entities/Enemy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatsJingleBang.Entities;
+public class Enemy
+{
+    public string Id { get; set; }
+    public Position pos { get; set; }
+}
